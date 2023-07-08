8e1e5e01e71d939e61180446dfb290a03b99789b
Andrey <araibov@mail.ru>
2023-07-08 13:40:09 +0500

    edit: переделал названия файлов

--- src/layout/AuthWrapper.jsx
+++ src/layout/AuthWrapper.jsx
@@ -1,12 +1,11 @@
 import { SimpleHeader } from "../components/SimpleHeader/SimpleHeader";
 import { Box, CircularProgress, Container } from "@mui/material";
 
-export function FormContainer({ form }) {
+export function AuthWrapper({ form }) {
 	return (
 		<Container>
 			<SimpleHeader />
 			<Box sx={{ mt: 14 }}>{form}</Box>
 		</Container>
 	);
-    
 }
