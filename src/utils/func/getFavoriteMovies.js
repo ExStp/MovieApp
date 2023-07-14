12cdf431d59f9829b4c20e64658afb2b24353733
Andrey <araibov@mail.ru>
2023-07-14 12:07:54 +0500

    edit: рефакторинг getFavoriteMovies.js

--- src/utils/func/getFavoriteMovies.js
+++ src/utils/func/getFavoriteMovies.js
@@ -3,16 +3,25 @@ import { API } from "../../services/TMDB/API";
 export async function getFavoriteMovies() {
 	let moviesArr = [];
 	const initPage = 1;
-	const firstRequest = await API.fetchGetFavoriteMovies(initPage);
-	const { total_pages, total_results } = firstRequest;
-	const firstIterationArr = firstRequest?.results.map((item) => item.id) || [];
 
-	for (let page = 2; page <= total_pages; page++) {
-		console.log(`${page} из ${total_pages}`);
+	try {
+		const firstRequest = await API.fetchGetFavoriteMovies(initPage);
+		if (!firstRequest) throw Error("NetworkError");
+		const { total_pages, total_results } = firstRequest;
+		const firstIterationArr = firstRequest?.results.map((item) => item.id) || [];
 
-		const iterationRequest = await API.fetchGetFavoriteMovies(page);
-		const iterationArr = iterationRequest?.results.map((movie) => movie.id) || [];
-		moviesArr = [...moviesArr, ...iterationArr];
+		for (let page = 2; page <= total_pages; page++) {
+			console.log(`${page} из ${total_pages}`);
+
+			const iterationRequest = await API.fetchGetFavoriteMovies(page);
+			const iterationArr = iterationRequest?.results.map((movie) => movie.id) || [];
+			moviesArr = [...moviesArr, ...iterationArr];
+		}
+		
+		moviesArr = [...moviesArr, ...firstIterationArr];
+	} catch (error) {
+		console.log(error.message);
+	} finally {
+		return moviesArr;
 	}
-	return [...moviesArr, ...firstIterationArr];
 }
