97bb36047dbbc4ab06c4b74c4dd611fda97d0e3b
Andrey <araibov@mail.ru>
2023-07-18 16:54:28 +0500

    edit: удалил контекст dialogsContext

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,13 +1,10 @@
 import { FiltersProvider } from "./FiltersProvider";
-import { DialogsProvider } from "./DialogsProvider";
 import { AuthProvider } from "./AuthProvider";
 
 export function AppProvider({ children }) {
 	return (
 		<AuthProvider>
-			<FiltersProvider>
-				<DialogsProvider>{children}</DialogsProvider>
-			</FiltersProvider>
+			<FiltersProvider>{children}</FiltersProvider>
 		</AuthProvider>
 	);
 }
