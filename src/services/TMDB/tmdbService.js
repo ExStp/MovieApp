b76ffbb93e6299f46567bfdda02fc8002aaf0a09
Andrey <araibov@mail.ru>
2023-07-20 12:50:18 +0500

    edit: добавил static class в tmdb service

--- src/services/TMDB/tmdbService.js
+++ src/services/TMDB/tmdbService.js
@@ -1,17 +1,38 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const apiToken =
-	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTZlODNjMTNjNmViMDQ0OTc3ZTk1NzFhY2U0M2U0MSIsInN1YiI6IjY0OTE3NTRjNDJiZjAxMDBlNGEwNTQ2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5cNOMjLZ0tL54dy8U0BiGOEIZjON-YsTtNrXy6A5OLQ";
+class API {
+	static URL = {
+		base: "https://api.themoviedb.org/3/",
+		IMG: {
+			W400: `https://image.tmdb.org/t/p/w400`,
+			W300: `https://image.tmdb.org/t/p/w300`,
+		},
+	};
 
-const baseUrl = "https://api.themoviedb.org/3/";
+	static config = {
+		token: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTZlODNjMTNjNmViMDQ0OTc3ZTk1NzFhY2U0M2U0MSIsInN1YiI6IjY0OTE3NTRjNDJiZjAxMDBlNGEwNTQ2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5cNOMjLZ0tL54dy8U0BiGOEIZjON-YsTtNrXy6A5OLQ",
+		accountId: 20036970,
+	};
 
-const accountId = 20036970;
+	static headers = {
+		accept: "application/json",
+		authorization: `Bearer ${API.config.token}`,
+	};
+
+	static error = {
+		CORS_ERROR: "Недоступен сервис API, перезапустите VPN",
+		AUTH_FALSE: "Необходима авторизация",
+	};
+}
 
 export const tmdbAPI = createApi({
 	reducerPath: "tmdbAPI",
 	baseQuery: fetchBaseQuery({
-		baseUrl,
-		prepareHeaders: (headers) => headers.set("authorization", `bearer ${apiToken}`),
+		baseUrl: API.URL.base,
+		prepareHeaders: (headers) => {
+			headers.set("accept", API.headers.accept);
+			headers.set("authorization", API.headers.authorization);
+		},
 	}),
 	endpoints: (build) => ({
 		fetchGetGenres: build.query({
@@ -19,12 +40,14 @@ export const tmdbAPI = createApi({
 		}),
 		fetchGetDetails: build.query({
 			query: (film_id) => `movie/${film_id}?language=ru`,
+			keepUnusedDataFor: 300,
 		}),
 		fetchGetCredits: build.query({
 			query: (film_id) => `movie/${film_id}/credits?language=ru`,
+			keepUnusedDataFor: 300,
 		}),
 		fetchGetAccountDetails: build.query({
-			query: () => `/account/${accountId}`,
+			query: () => `/account/${API.config.accountId}`,
 		}),
 	}),
 });
