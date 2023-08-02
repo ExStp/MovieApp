6f7847a8e7eeb94d20f1108642e22d5febcf8e57
Andrey <araibov@mail.ru>
2023-08-02 13:38:39 +0500

    edit: улучшил синтаксис тестов

--- src/utils/tests/formatData.test.js
+++ src/utils/tests/formatData.test.js
@@ -1,13 +1,20 @@
 import { getMappedArr, getFormattedBudget } from "../func/formatData";
+import { test } from "@jest/globals";
 
 describe("test getMappedArr", () => {
-	test("empty arr", () => expect(getMappedArr([])).toEqual(null));
-	test("empty string", () => expect(getMappedArr("")).toEqual(null));
-	test("null", () => expect(getMappedArr()).toEqual(null));
+	test.each([
+		{ a: [], expected: null },
+		{ a: "", expected: null },
+		{ a: null, expected: null },
+	])("test № %#", ({ a, expected }) => {
+		expect(getMappedArr(a)).toEqual(expected);
+	});
 });
-
 describe("test getFormattedBudget", () => {
-	test("empty string", () => expect(getFormattedBudget("")).toEqual(null));
-	test("num zero", () => expect(getFormattedBudget(0)).toEqual(null));
-	test("empty arr", () => expect(getFormattedBudget([])).toEqual(null));
+	test.each([
+		{ a: "", expected: null },
+		{ a: 0, expected: null },
+	])("test № %#", ({ a, expected }) => {
+		expect(getFormattedBudget(a)).toEqual(expected);
+	});
 });
